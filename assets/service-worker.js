f715217e63a469de2ba16803b23dc43591f5dcc4
ferdinand-koenig <65330418+ferdinand-koenig@users.noreply.github.com>
2025-08-24 22:28:30 +0200

    foreground service-worker.js

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -20,3 +20,41 @@ self.addEventListener('message', (event) => {
         });
     }
 });
+
+self.addEventListener('push', (event) => {
+    let data = {};
+    if (event.data) {
+        data = event.data.json();
+    }
+
+    const title = data.title || "InsightBridge";
+    const options = {
+        body: data.body || "Your answer is ready!",
+        icon: data.icon || "/favicon.ico"
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(title, options)
+    );
+});
+
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(function(clientList) {
+            if (clientList.length > 0) {
+                let client = clientList[0];
+                for (let i = 0; i < clientList.length; i++) {
+                    if (clientList[i].focused) {
+                        client = clientList[i];
+                        break;
+                    }
+                }
+                return client.focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
